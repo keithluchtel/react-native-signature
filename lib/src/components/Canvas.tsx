030d9418cc677483e1e8fb7f929c0ac7534b9a41
Keith Luchtel <keith@keithluchtel.com>
2024-01-31 21:07:22 -0600

    Retain cache of all previous strokes

--- lib/src/components/Canvas.tsx
+++ lib/src/components/Canvas.tsx
@@ -1,12 +1,33 @@
 import {StyleSheet} from "react-native";
-import {Canvas as SkiaCanvas, Path} from '@shopify/react-native-skia'
-import {useSharedValue} from "react-native-reanimated";
+import {
+  Canvas as SkiaCanvas,
+  createPicture,
+  PaintStyle,
+  Path, Picture,
+  Skia,
+  type SkPath,
+  StrokeCap
+} from '@shopify/react-native-skia'
+import {useDerivedValue, useSharedValue} from "react-native-reanimated";
 import {Gesture, GestureDetector} from "react-native-gesture-handler";
 
 export const Canvas = () => {
 
+  const cachedStrokes = useSharedValue<SkPath[]>([]);
   const currentStroke = useSharedValue('');
 
+  const cachedPicture = useDerivedValue(() => (
+    createPicture(canvas => {
+      const paint = Skia.Paint();
+      paint.setStrokeWidth(2);
+      paint.setStrokeCap(StrokeCap.Round);
+      paint.setStyle(PaintStyle.Stroke);
+
+      for (const stroke of cachedStrokes.value) {
+        canvas.drawPath(stroke, paint);
+      }
+    })
+  ), [cachedStrokes]);
   const panGesture = Gesture.Pan()
     .onBegin((e) => {
       currentStroke.value = `M ${e.x} ${e.y} L ${e.x} ${e.y}`
@@ -15,12 +36,17 @@ export const Canvas = () => {
       currentStroke.value += ` L ${e.x} ${e.y}`
     })
     .onEnd(() => {
+      cachedStrokes.value = [
+        ...cachedStrokes.value,
+        Skia.Path.MakeFromSVGString(currentStroke.value) as SkPath
+      ];
       currentStroke.value = '';
     });
 
   return (
     <GestureDetector gesture={panGesture}>
       <SkiaCanvas style={styles.canvas}>
+        <Picture picture={cachedPicture} />
         <Path path={currentStroke} color="black" strokeWidth={2} style="stroke"/>
       </SkiaCanvas>
     </GestureDetector>
